34bbf70ed0b432232ca1c61a682ae95fdb32eabf
lukasz <luk.walczak1@gmail.com>
2025-12-01 10:27:03 +0100

    Add validator comments

--- warhammer-list-creator/src/main/java/pl/edu/pg/eti/kask/list/army/model/ArmyCreateModel.java
+++ warhammer-list-creator/src/main/java/pl/edu/pg/eti/kask/list/army/model/ArmyCreateModel.java
@@ -18,11 +18,14 @@ import java.util.UUID;
 @ToString
 @EqualsAndHashCode
 public class ArmyCreateModel implements Serializable {
+
     private UUID id;
+
     @NotBlank
     @Size(min = 1, max = 100, message = "Name must be between 1 and 100 characters long")
     @ForbiddenWordsValidatorApi(message = "Name contains forbidden words")
     private String name;
+
     @Size(max = 1000, message = "Opis armii nie może przekraczać 1000 znaków")
     private String description;
 
--- warhammer-list-creator/src/main/java/pl/edu/pg/eti/kask/list/validation/api/ForbiddenWordsValidatorApi.java
+++ warhammer-list-creator/src/main/java/pl/edu/pg/eti/kask/list/validation/api/ForbiddenWordsValidatorApi.java
@@ -1,7 +1,17 @@
 package pl.edu.pg.eti.kask.list.validation.api;
 
+import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import pl.edu.pg.eti.kask.list.validation.impl.ForbiddenWordsValidator;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType. FIELD, ElementType. METHOD, ElementType. PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ForbiddenWordsValidator.class)
 public @interface ForbiddenWordsValidatorApi {
 
     String message() default "Forbidden words detected";
@@ -10,5 +20,5 @@ public @interface ForbiddenWordsValidatorApi {
 
     Class<?  extends Payload>[] payload() default {};
 
-    String[] forbiddenWords() default {"Dupa", "Kupa", "Chuj", "Pizda"};
+    String[] forbiddenWords() default {};
 }
